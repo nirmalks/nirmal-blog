0cfe5ad8cb9e52d41a6d559de72d02fbd435efbb
Nirmal Kumar <kagosekar@gmail.com>
2020-05-24 16:55:46 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Nirmal`,
+    author: `Nirmal Kumar Sekar`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `nirmalche`,
     },
   },
   plugins: [
